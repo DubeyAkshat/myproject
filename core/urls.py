ef6e8f925b9ecd022a7cc94674618a7bffd93288
Akshat Dubey <iakshatdubey@gmail.com>
2023-09-24 10:57:33 +0530

    Implemented Swagger Documentation

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import AllCarsView, CarView
 
+# Swagger
+from rest_framework_swagger.views import get_swagger_view
+schema_view = get_swagger_view(title='Cars API')
+
 urlpatterns = [
+    path('swagger/', schema_view),
     path('cars/', AllCarsView.as_view(), name='Create Car'),
     path('cars/<int:id>/', CarView, name='Car'),
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
     'core',
 ]
 
@@ -85,6 +86,9 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = { 
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
