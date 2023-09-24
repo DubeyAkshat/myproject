f3da1f14d968641dfdab3a2bc03e65ed7103de57
Akshat Dubey <iakshatdubey@gmail.com>
2023-09-24 11:50:16 +0530

    Implemented drf_yasg

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,19 @@ from django.urls import path
 from .views import AllCarsView, CarView
 
 # Swagger
-from rest_framework_swagger.views import get_swagger_view
-schema_view = get_swagger_view(title='Cars API')
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Cars API",
+      default_version='v1',
+   ),
+   public=True,
+)
 
 urlpatterns = [
-    path('swagger/', schema_view),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('cars/', AllCarsView.as_view(), name='Create Car'),
     path('cars/<int:id>/', CarView, name='Car'),
 ]
